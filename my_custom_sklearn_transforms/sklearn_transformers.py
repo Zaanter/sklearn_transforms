b2f9d8320c0c50ffc19a412b268836fa4a8992c1
Agustin Troche <44510707+Zaanter@users.noreply.github.com>
2020-12-05 15:53:57 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import numpy as pd
 
 
 
@@ -14,29 +14,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
+        data = pd.get_dummies(data, columns=['koi_pdisposition'])
         # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
         return data.drop(labels=self.columns, axis='columns')
     
-
-
-class oversampling(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        self
-
-    def fit(self, X,y=None):
-        return self
-
-    def transform(self, X):
-        # Primero copiamos el dataframe de entrada 'X' de entrada
-        data = X.copy()
-
-        dataA = data[data['OBJETIVO'] == 'Aceptado']
-        dataA = dataA.sample(4500, replace=True)
-
-        dataB = data[data['OBJETIVO'] == 'Sospechoso']
-        dataB = dataB.sample(4500, replace=True)
-
-        data = pd.concat([dataA,dataB])
-
-        return data
-        # Devolvemos un nuevo marco de datos sin las columnas no deseadas
